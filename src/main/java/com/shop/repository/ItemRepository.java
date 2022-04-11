bfb3be731e4996638f71dabb1a330631be59b9bf
jaewan shin <wodhks6307@naver.com>
2022-04-11 23:57:15 +0900

    @Query/Queryds구현

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,18 +3,30 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 /*JpaRepository는 2개의 제네릭 타입을 사용하는데 첫 번째에는 엔티티 타입 클래스를 넣고, 두 반째는 기본키
 타입을 넣어준다. JpaRepository는 기본적인 CRUD 및 페이징 처리를 위한 메소드가 정의돼 있다.*/
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findByItemNm(String itemNm); //상품을 상품명으로 조회하는 쿼리 메소드
-    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail); //상품을 상품명과 상세 설명을 OR 조건을 이용하여 조회하는 쿼리 메소드
-    List<Item> findByPriceLessThan(Integer price); //파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
-    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); //price 변수보다 값이 작은 상품 데이터를 가격이 높은 순으로 조회
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    //상품을 상품명으로 조회하는 쿼리 메소드
+    List<Item> findByItemNm(String itemNm);
 
-    @Query("select i from  Item i where i.itemDetail like %:itemDetail% order by i.price desc")
-    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    //상품을 상품명과 상세 설명을 OR 조건을 이용하여 조회하는 쿼리 메소드
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    //파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThan(Integer price);
+
+    //price 변수보다 값이 작은 상품 데이터를 가격이 높은 순으로 조회
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc") //JPQL로 작성한 쿼리문(복잡한 쿼리의 경우 @Query 어노테이션을 사용하면 됨)
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail); //어노테이션을 이용하여 파라미터로 넘어온값을 JPQL에 들어갈 변수로 지정해줄 수 있음
+
+    //기존의 db에서 사용하던 쿼리를 그대로 사용해야 할 때는 @Query의 nativeQuery속성을 사용하면 기존 쿼리를 그대로 활용할 수 있음
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,13 +1,23 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -20,6 +30,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     //테스트 코드 실행시 @DisplayName에 지정한 테스트명이 노출됨
@@ -89,6 +102,103 @@ class ItemRepositoryTest {
         List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
         for(Item item : itemList){
             System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNativeTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1") //Querydsl은 SQL문을 문자열이 아닌 자바 코드로 작성하기 때문에 컴파일 시점에 오류를 발견할 수 있음
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); //JPAQueryFactory를 이용하여 쿼리를 동적으로 생성
+        QItem qItem = QItem.item; //Querydsl을 통해 쿼리를 생성하기 위해 플러그인을 통해 자동으로 생성된 QItem 객체를 이용한다.
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem) //자바 소스코드지만 SQL문과 비슷하게 소스를 작성할 수 있음
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch(); //JPAQuery 메소드중 하나인 fetch를 이용해서 쿼리 결과를 리스트로 반환
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    public void createItemList2(){
+        for(int i=1; i<=5; i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+
+        for(int i=6; i<=10; i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트2") //Querydsl은 SQL문을 문자열이 아닌 자바 코드로 작성하기 때문에 컴파일 시점에 오류를 발견할 수 있음
+    public void queryDslTest2(){
+        this.createItemList2();
+
+        //BooleanBuilder는 쿼리에 들어갈 조건을 만들어주는 빌더이다. Predicate를 구현하고 있으며 메소드 체인 형식으로 사용할 수 있음
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%")); //필요한 상품을 조회하는데 필요한 "and" 조건을 추가하고 있음
+        booleanBuilder.and(item.price.gt(price));
+
+        //상품의 판매상태가 SELL일때만 booleanBuilder에 판매상태 조건을 동적으로 추가
+        if(StringUtils.equals(itemSellStat, ItemSellStatus.SELL)){
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        /*데이터를 페이징에 조회하도록 PageRequest.of() 메소드를 이용해 Pageable 객체를 생성
+        첫번째 인자는 조회할 페이지의 번호, 두번째 인자는 한 페이지당 조회할 데이터의 개수를 넣어줌*/
+        Pageable pageable = PageRequest.of(0, 5);
+        //QuerydslPredicateExecutor 인터페이스에서 정의한 findAll() 메소드를 이용해 조건에 맞는 데이터를 Page 객체로 받아온다.
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " + itemPagingResult. getTotalElements ());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for(Item resultItem: resultItemList){
+            System.out.println(resultItem.toString());
         }
     }
 }
